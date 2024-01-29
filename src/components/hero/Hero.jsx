cd8dd981380c55e706959e2f4375bf985201043f
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-01-29 05:29:20 -0500

    Hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import styles from "./Hero.module.css";
+import bgi1 from "../../assets/background-img1.png";
+import bgi2 from "../../assets/background-img2.png";
 
 const Hero = () => {
   return (
-<section>hero</section>
-  )
-}
+    <section className={styles.hero}>
+      <div className={styles.background}>
+        <img className="test1" src={bgi1} alt="" />
+        <img className="test2" src={bgi2} alt="" />
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
