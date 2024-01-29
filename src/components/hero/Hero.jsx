ac757962f3301bbabaa40eb4094f696c5fc7782a
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-01-29 04:48:39 -0500

    Hero
    Redoing this section.

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,33 +1,9 @@
-import React from "react";
-import styles from "./Hero.module.css";
-import bgi1 from "../../assets/bgi1.png";
-import bgi2 from "../../assets/bgi2.png";
-import bannerText from "../../assets/company.png";
+import React from 'react'
 
 const Hero = () => {
   return (
-    <section className={styles.hero}>
-      <div></div>
-      <div></div>
-      <div className={styles.heroLogo}>
-        <div className={styles.heroText} style={{ paddingLeft: "0rem" }}>
-          WEB
-        </div>
-        <div className={styles.heroText} style={{ paddingLeft: "10.94rem" }}>
-          DE
-        </div>
-        <div className={styles.heroText} style={{ paddingLeft: "20.06rem" }}>
-          SIG
-        </div>
-        <div className={styles.heroAsset}>
-          <div className={styles.banner}>
-            <img className={styles.bannerText} src={bannerText} alt="" />
-          </div>
-          <div className={styles.heroText}>N</div>
-        </div>
-      </div>
-    </section>
-  );
-};
+<section>hero</section>
+  )
+}
 
-export default Hero;
+export default Hero
