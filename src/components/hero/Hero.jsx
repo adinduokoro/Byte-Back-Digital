e6e3894b634cd83cceade38fc933493f7de875ef
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-01-29 02:14:49 -0500

    Hero
    Need to add the two background images and make it responsive.

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
-import styles from "./Hero.module.css"
+import React from "react";
+import styles from "./Hero.module.css";
+import bgi1 from "../../assets/bgi1.png";
+import bgi2 from "../../assets/bgi2.png";
+import heroBanner from "../../assets/heroBanner.png";
 
 const Hero = () => {
   return (
     <section className={styles.hero}>
-        {/* background image 1 */}
-        {/* background image 2 */}
-        {/* animation logo */}
+      <div className={styles.heroLogo}>
+        <div className={styles.heroText} style={{ paddingLeft: "0rem" }}>
+          WEB
+        </div>
+        <div className={styles.heroText} style={{ paddingLeft: "10.94rem" }}>
+          DE
+        </div>
+        <div className={styles.heroText} style={{ paddingLeft: "20.06rem" }}>
+          SIG
+        </div>
+        <div className={styles.heroAsset}>
+          <div className={styles.banner}>
+            <img src={heroBanner} alt=""/>
+          </div>
+          <div className={styles.heroText}>N</div>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
