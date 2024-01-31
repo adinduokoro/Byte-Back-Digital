ae48dc7fd0a95260e46e7e700db453674946ec85
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-01-30 22:47:45 -0500

    Marquee
    The marquee animation is complete. Next is the content of the offer section.

--- src/components/offer/Offer.jsx
+++ src/components/offer/Offer.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import styles from "./Offer.module.css"
+import React from "react";
+import styles from "./Offer.module.css";
 
 const Offer = () => {
   return (
     <section className={styles.offer}>
-      hello
+      <div className={styles.marquee}>
+        <div className={styles["marquee-text"]}>
+          Byte <span>Back</span> Digital
+        </div>
+        <div className={styles["marquee-text"]}>
+          Byte <span>Back</span> Digital
+        </div>
+        <div className={styles["marquee-text"]}>
+          Byte <span>Back</span> Digital
+        </div>
+        <div className={styles["marquee-text"]}>
+          Byte <span>Back</span> Digital
+        </div>
+        <div className={styles["marquee-text"]}>
+          Byte <span>Back</span> Digital
+        </div>
+      </div>
+      <div className={styles["offer-content"]}>content</div>
     </section>
-  )
-}
+  );
+};
 
-export default Offer
+export default Offer;
