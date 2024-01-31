33a7df43b67672f033da149297a6b5eace5bf9c9
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-01-31 00:25:50 -0500

    Offer
    Offer section html structure is being created. Still need to make figma version in order to get dimensions. Then ill work on responsiveness.

--- src/components/offer/Offer.jsx
+++ src/components/offer/Offer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Offer.module.css";
+import arrow from "../../assets/arrow-right.svg";
 
 const Offer = () => {
   return (
@@ -14,14 +15,22 @@ const Offer = () => {
         <div className={styles["marquee-text"]}>
           Byte <span>Back</span> Digital
         </div>
-        <div className={styles["marquee-text"]}>
-          Byte <span>Back</span> Digital
-        </div>
-        <div className={styles["marquee-text"]}>
-          Byte <span>Back</span> Digital
-        </div>
       </div>
-      <div className={styles["offer-content"]}>content</div>
+      <div className={styles["offer-content"]}>
+        <div className={styles.left}>
+          <div className={`${styles.subHeading} subHead-text`}>
+            <img className={styles.icon} src={arrow} alt="" />
+            What We Do
+          </div>
+          <div className={`${styles.heading} d-text`}>
+            Service That Makes Your Brand Stand Out and Grow
+          </div>
+          <div className={styles["offer-cards"]}>
+            {/* create card */}
+          </div>
+        </div>
+        <div className={styles.right}>img</div>
+      </div>
     </section>
   );
 };
