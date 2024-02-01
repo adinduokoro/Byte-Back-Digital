9a4f9a5138395ac1ecdac374cf5cd7652e0be5f8
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-02-01 00:28:03 -0500

    Gallery
    Update

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -5,21 +5,21 @@ import arrow from "../../assets/arrow-right.svg";
 const Gallery = () => {
   return (
     <section className={styles.gallery}>
-      <div className={styles["header-content"]}>
-        <div className={`${styles.subHeading} subHead-text`}>
-          <img className={styles.icon} src={arrow} alt="" />
-          Recent Works Gallery
-        </div>
-        <div className={styles["heading-content"]}>
-          <div className={`${styles.heading} d-text`}>
-            Take a Look at Our Recent Project Gallery
-          </div>
-          <button className={`${styles.button} link-text`}>
-            View More Projects
-          </button>
-        </div>
+      <div className={`${styles.subHeading} subHead-text`}>
+        <img className={styles.icon} src={arrow} alt="" />
+        Recent Works Gallery
       </div>
-      <div className={styles.gallery}></div>
+      <div className={styles["heading-content"]}>
+        <div className={`${styles.heading} d-text`}>
+          Take a Look at Our Recent Project Gallery
+        </div>
+        <button className={`${styles.button} link-text`}>
+          View More Projects
+        </button>
+      </div>
+      <div className={styles.gallery}>
+        gallery pics
+      </div>
     </section>
   );
 };
