7b13b21869c49a08f5c6daad13d66f29d63bf31f
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-01-31 21:23:43 -0500

    Service
    Service section is all done. Might add some hover animation to it to make it unique.

--- src/components/service/data.js
+++ src/components/service/data.js
@@ -1,9 +1,9 @@
-import figma from "../../assets/figma-icon.svg"
-import html from "../../assets/html-icon.svg"
-import react from "../../assets/react-icon.svg"
+import figma from "../../assets/figma-icon.svg";
+import html from "../../assets/html-icon.svg";
+import react from "../../assets/react-icon.svg";
 
 export const badges = [
   { image: react, topic: "React Development" },
-  { image: figma, topic: "We Design Created in Figma" },
+  { image: figma, topic: "Web Design Created in Figma" },
   { image: html, topic: "PSD, Figma, XD to HTML Convert" },
 ];
