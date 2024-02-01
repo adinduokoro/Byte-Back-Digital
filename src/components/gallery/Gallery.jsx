63f8e724d23b769a97fb8e51cd8b9e7712cc77ed
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-02-01 00:25:22 -0500

    Gallery
    Update/Checkpoint

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import styles from "./Gallery.module.css"
+import React from "react";
+import styles from "./Gallery.module.css";
+import arrow from "../../assets/arrow-right.svg";
 
 const Gallery = () => {
   return (
     <section className={styles.gallery}>
-        Gallery
+      <div className={styles["header-content"]}>
+        <div className={`${styles.subHeading} subHead-text`}>
+          <img className={styles.icon} src={arrow} alt="" />
+          Recent Works Gallery
+        </div>
+        <div className={styles["heading-content"]}>
+          <div className={`${styles.heading} d-text`}>
+            Take a Look at Our Recent Project Gallery
+          </div>
+          <button className={`${styles.button} link-text`}>
+            View More Projects
+          </button>
+        </div>
+      </div>
+      <div className={styles.gallery}></div>
     </section>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
