8583e809e9f449b12736eb6dfe84173d20d35956
Addie Okoro <adinduokoro95@gmail.com>
2024-02-03 12:00:28 -0500

    Gallery
    Basically Done

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -2,24 +2,41 @@ import React from "react";
 import styles from "./Gallery.module.css";
 import arrow from "../../assets/arrow-right.svg";
 import { projects } from "./data";
+import linkicon from "../../assets/link-icon.svg"
 
 const Gallery = () => {
   return (
     <section className={styles.gallery}>
-      <div className={`${styles.subHeading} subHead-text`}>
-        <img className={styles.icon} src={arrow} alt="" />
-        Recent Works Gallery
-      </div>
-      <div className={styles["heading-content"]}>
-        <div className={`${styles.heading} d-text`}>
-          Take a Look at Our Recent Project Gallery
+      <div className={styles["gallery-header"]}>
+        <div className={`${styles.subHeading} subHead-text`}>
+          <img className={styles.icon} src={arrow} alt="" />
+          Recent Works Gallery
         </div>
-        <button className={`${styles.button} link-text`}>
-          View More Projects
-        </button>
+        <div className={styles["heading-content"]}>
+          <div className={`${styles.heading} d-text`}>
+            Take a Look at Our Recent Project Gallery
+          </div>
+          <button className={`${styles.button} link-text`}>
+            View More Projects
+          </button>
+        </div>
       </div>
 
-      <div className={styles.gallery}>gallery pics, need to learn grid</div>
+      <div className={styles["gallery-container"]}>
+        {projects.map((project, index) => {
+          return (
+            <div className={styles.project} key={index}>
+              <div className={styles["project-container"]}>
+                <img src={project.image} alt="" />
+              </div>
+              <div className={`${styles.title} title-text`}>
+                {project.title}
+              <img src={linkicon} alt="" />
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 };
