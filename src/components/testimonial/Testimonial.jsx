6a9d3bb1169d4bbb696d2c2ee124579a151fa490
Addie Okoro <adinduokoro95@gmail.com>
2024-02-03 13:47:31 -0500

    Testimonial
    Working on the html structure.

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import styles from "./Testimonials.module.css"
+import React from "react";
+import styles from "./Testimonials.module.css";
 
 const Testimonial = () => {
   return (
-    <section className={styles.testimonial}>Testimonial</section>
-  )
-}
+    <section className={styles.testimonial}>
+      <div className={styles.marquee}>
+        <div className={styles["marquee-text"]}>
+          CLIENTS <span>SAY</span>
+        </div>
+        <div className={styles["marquee-text"]}>
+          CLIENTS <span>SAY</span>
+        </div>
+        <div className={styles["marquee-text"]}>
+          CLIENTS <span>SAY</span>
+        </div>
+        <div className={styles["testimonial-content"]}>
+          <div className={styles.left}>
 
-export default Testimonial
+          </div>
+          <div className={styles.right}>
+            
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonial;
