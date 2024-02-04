f0cd24dc7633ee87ef0a26261cc75db0559c8d49
Addie Okoro <adinduokoro95@gmail.com>
2024-02-04 17:02:39 -0500

    Testimonials
    Working on finishing the styling

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Testimonials.module.css";
+import clientPic from "../../assets/client-picture.png";
 
 const Testimonial = () => {
   return (
@@ -14,13 +15,37 @@ const Testimonial = () => {
         <div className={styles["marquee-text"]}>
           CLIENTS <span>SAY</span>
         </div>
-        <div className={styles["testimonial-content"]}>
-          <div className={styles.left}>
-
+      </div>
+      <div className={styles["testimonial-content"]}>
+        <div className={styles.left}>
+          <div className={styles.review}>
+            <div className={styles.rating}> 5 stars</div>
+            <div className={`${styles.feedback} subHead-text`}>
+              Incredible custom design! The team's dedication, creativity, and
+              attention to detail exceeded our expectations. Highly recommend
+              their top-notch service!
+            </div>
           </div>
-          <div className={styles.right}>
-            
+          <div className={styles.info}>
+            <div className={`${styles.name} title-text`}>Jane Doe</div>
+            <div className={`${styles.title} body-text`}>CEO & Founder</div>
+            <div className={styles["slide-controls"]}>
+              <div className={styles["left-button"]}></div>
+              <div className={styles.index}>
+                <div className={styles.index1}></div>
+                <div className={styles.index2}></div>
+                <div className={styles.index3}></div>
+              </div>
+              <div className={styles["right-button"]}>
+                <div className={styles["image-container"]}>
+                  <img src="" alt="" />
+                </div>
+              </div>
+            </div>
           </div>
+        </div>
+        <div className={styles.right}>
+          <img className={styles["client-pic"]} src={clientPic} alt="" />
         </div>
       </div>
     </section>
