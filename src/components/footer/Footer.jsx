961a157a304a7da35e1c0fa2c37a1009ee2ba479
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-02-05 02:28:49 -0500

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,24 @@ const Footer = () => {
   return (
     <footer>
       <section className={styles.footer}>
-        
+        <div className={styles["footer-top"]}>
+          Logo 
+          Socials
+        </div>
+        <div className={styles["footer-bottom"]}>
+          <div className={styles.left}>
+            Header 
+            Contact
+          </div>
+          <div className={styles.right}>
+            <div className={styles["bottom-right-top"]}>
+              Quick Links
+            </div>
+            <div className={styles["bottom-right-bottom"]}>
+              Copyright
+            </div>
+          </div>
+        </div>
       </section>
     </footer>
   );
