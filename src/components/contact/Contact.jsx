df4b9b8196223ee3733a2431dda83d36e676af62
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-02-05 00:23:01 -0500

    Contact
    Half way done the HTML and CSS structure of the contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,55 @@
 import React from "react";
 import styles from "./Contact.module.css";
+import arrow from "../../assets/arrow-right.svg";
+import { contacts } from "./data";
 
 const Contact = () => {
   return (
     <section className={styles.contact}>
-      <div className={styles.left}></div>
-      <div className={styles.right}></div>
+      <div className={styles.left}>
+        <div className={`${styles.subHeading} subHead-text`}>
+          <img className={styles.icon} src={arrow} alt="" />
+          Contact Us
+        </div>
+        <div className={`${styles.heading} d-text`}>
+          Ready to Work <span>your</span> Next Project{" "}
+        </div>
+        <div className={styles.contacts}>
+          {contacts.map((contact, index) => {
+            return (
+              <div className={styles["contact-card"]} key={index}>
+                <img src={contact.img} alt="" />
+                <div className={`${styles["contact-type"]} title-text`}>
+                  {contact.title}
+                </div>
+                <div className={`${styles.method} body-text`}>
+                  <span>{contact.method}:</span> {contact.details}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className={styles.socials}>
+          <div className={`${styles["follow-us"]} title-text`}>Follow Us</div>
+          Links
+        </div>
+      </div>
+      <div className={styles.right}>
+        <form action="">
+          <div className={styles["form-header"]}>
+            <div className={`${styles.subHeading} subHead-text`}>
+              <img className={styles.icon} src={arrow} alt="" />
+              Contact Us
+            </div>
+            <div className={`${styles.heading} d-text`}>Drop a Message</div>
+          </div>
+
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+        </form>
+      </div>
     </section>
   );
 };
---
+++ src/components/contact/data.js
@@ -0,0 +1,17 @@
+import phone from "../../assets/fa-mobile.svg"
+import pencil from "../../assets/fa-pencil.svg"
+
+export const contacts = [
+  {
+    img: phone,
+    title: "Call Us",
+    method: "Support",
+    details: "1 (800) 123-4567",
+  },
+  {
+    img: pencil,
+    title: "Write Us",
+    method: "Email",
+    details: "example@gmail.com",
+  },
+];
