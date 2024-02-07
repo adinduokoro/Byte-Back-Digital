c15ec2a9db7529948ff7e8b26b04c2d95ebec18d
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-02-07 00:48:36 -0500

    Navigation

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,29 +1,31 @@
-import React from 'react'
-import "./Navigation.module.css"
-import styles from "./Navigation.module.css"
-import logo from "../../assets/logo.svg"
-import menuToggle from "../../assets/menuToggle.svg"
-import plusIcon from '../../assets/plus-icon.svg'
+import React from "react";
+import "./Navigation.module.css";
+import styles from "./Navigation.module.css";
+import logo from "../../assets/logo.svg";
+import menuToggle from "../../assets/menuToggle.svg";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <section className={styles.navigation}>
-      <img className={styles.logo} src={logo} alt="logo" />
-      <ul className={styles.navLinks}>
-        <li className={`${styles.navLink} link-text`}>Home</li>
-        <li className={`${styles.navLink} link-text`}>About Us</li>
-        <li className={`${styles.navLink} link-text`}>Portfolio</li>
-        <li className={`${styles.navLink} link-text`}>
-          Services
-          <img className={styles.dropdown} src={plusIcon} alt=''/>
-        </li>
-        <li className={`${styles.navLink} link-text`}>Contact</li>
-      </ul>
-      <div className={styles.menuToggle}>
-        <img className={styles.toggle} src={menuToggle} alt="menu" />
+      <div className={styles["nav-container"]}>
+        <Link to="/">
+          <img className={styles.logo} src={logo} alt="logo" />
+        </Link>
+
+        <ul className={styles.navLinks}>
+          <li className={`${styles.navLink} link-text`}><Link to="/">Home</Link></li>
+          <li className={`${styles.navLink} link-text`}><Link>About Us</Link></li>
+          <li className={`${styles.navLink} link-text`}><Link>Portfolio</Link></li>
+          <li className={`${styles.navLink} link-text`}><Link>Services</Link></li>
+          <li className={`${styles.navLink} link-text`}><Link to="/contact">Contact</Link></li>
+        </ul>
+        <div className={styles.menuToggle}>
+          <img className={styles.toggle} src={menuToggle} alt="menu" />
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Navigation 
+export default Navigation;
