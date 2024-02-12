854c8a4752594ab378d27052db9d24999c3e659b
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-02-12 00:28:44 -0500

    Hero
    The hero animation is basically set and done.

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -6,58 +6,102 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 
 const Hero = () => {
-  const hero = useRef();
-  const tl = useRef();
+  const container = useRef();
+  const timeline = useRef();
 
   useGSAP(
     () => {
-      tl.current = gsap.timeline()
+      timeline.current = gsap
+        .timeline()
+        .from("#bg1", {
+          delay: 0.6,
+          duration: 1.2,
+          xPercent: -110,
+          ease: "power2.inOut",
+        })
+        .from(
+          "#bg2",
+          {
+            duration: 1.2,
+            xPercent: 110,
+            ease: "power1",
+          },
+          ">-1.2"
+        )
+        .from("#web-text", { stagger: 0.15, y: -100, opacity: 0 }, ">=-0.2")
+        .from(
+          "#de",
+          {
+            stagger: 0.15,
+            x: 100,
+            opacity: 0,
+          },
+          ">=-0.3"
+        )
+        .from(
+          "#sig",
+          {
+            stagger: 0.15,
+            y: -100,
+            opacity: 0,
+          },
+          ">=-0.4"
+        )
+        .from(
+          "#n-text",
+          {
+            stagger: 0.15,
+            y: 100,
+            opacity: 0,
+          },
+          ">=-0.4"
+        )
+        .from(
+          "#bar",
+          {
+            stagger: 0.15,
+            x: -300,
+            opacity: 0,
+          },
+          "<"
+        );
     },
-    { scope: hero }
+    { scope: container }
   );
 
   return (
-    <section className={styles.hero} ref={hero}>
+    <section className={styles.hero} ref={container}>
       <div className={styles["hero-content"]}>
         <div className={styles["hero-logo"]}>
-          <div
-            className={styles.text}
-            style={{ marginLeft: "0" }}
-            id="hero-text"
-          >
-            <span id="single-letter">W</span>
-            <span id="single-letter">E</span>
-            <span id="single-letter">B</span>
+          <div className={styles.text} style={{ marginLeft: "0" }}>
+            <div id="web-text">W</div>
+            <div id="web-text">E</div>
+            <div id="web-text">B</div>
           </div>
-          <div
-            className={styles.text}
-            style={{ marginLeft: "8.69rem" }}
-            id="hero-text"
-          >
-            DE
+          <div className={styles.text} style={{ marginLeft: "8.69rem" }}>
+            <div id="de">D</div>
+            <div id="de">E</div>
           </div>
-          <div
-            className={styles.text}
-            style={{ marginLeft: "16.06rem" }}
-            id="hero-text"
-          >
-            <span id="single-leter">S</span>
-            <span id="single-leter">I</span>
-            <span id="single-ltter">G</span>
+          <div className={styles.text} style={{ marginLeft: "16.06rem" }}>
+            <div id="sig">S</div>
+            <div id="sig">I</div>
+            <div id="sig">G</div>
           </div>
 
           <div className={styles["text-content"]}>
-            <div className={styles.bar}>
+            <div className={styles.bar} id="bar">
               <div className={styles.tag}>COMPANY</div>
             </div>
-            <div className={styles.text}>N</div>
+            <div className={styles.text} id="n-text">
+              N
+            </div>
           </div>
         </div>
         <div className={styles.left}>
-          <img className={styles.bg1} src={bg1} id="bg1" alt="" />
+          <img className={styles.bg1} src={bg1} alt="" id="bg1" />
         </div>
         <div className={styles.right}>
-          <img className={styles.bg2} src={bg2} id="bg2" alt="" />
+          <img className={styles.bg2} src={bg2} alt="" id="bg2" />
         </div>
       </div>
     </section>
