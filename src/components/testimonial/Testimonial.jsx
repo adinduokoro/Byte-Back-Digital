734ba511f85558c0c1efc427e162522f9f8ef14a
Addie Okoro <adinduokoro95@gmail.com>
2024-02-18 13:10:17 -0500

    Update Testimonial.jsx

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -74,16 +74,15 @@ const Testimonial = () => {
               alt=""
             />
             <div className={styles.index}>
-
-
+              {/*  */}
+              {/*  */}
               <div
                 className={`${styles["point-active"]} ${styles.point}`}
               ></div>
               <div className={styles.point}></div>
               <div className={styles.point}></div>
-
-
-              
+              {/*  */}
+              {/*  */}
             </div>
             <img
               className={styles["right-button"]}
