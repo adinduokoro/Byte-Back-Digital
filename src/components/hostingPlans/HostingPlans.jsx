90925008de70b172bc73e6d57380ddf8d594cb53
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-03-06 22:46:45 -0500

    Update

---
+++ src/components/hostingPlans/HostingPlans.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./HostingPlans.module.css"
+
+const HostingPlans = () => {
+  return (
+    <div className={styles.hostingPlans}>HostingPlans</div>
+  )
+}
+
+export default HostingPlans
